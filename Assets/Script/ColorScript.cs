9eb13074c09e2002f2c7cd727ea9fda99ca65634
Ankit <ankittrajj1996@gmail.com>
2019-08-06 00:07:10 +0530

    e-commerce AR project

---
+++ Assets/Script/ColorScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorScript : MonoBehaviour
+{
+    public Material[] bodyColorMat;
+    Material currentMat;
+    Renderer renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = this.GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void blueColor()
+    {
+        renderer.material = bodyColorMat[0];
+        currentMat = renderer.material;
+    }
+
+         public void blackColor()
+        {
+        renderer.material = bodyColorMat[1];
+        currentMat = renderer.material;
+        }
+        public void redColor()
+        {
+        renderer.material = bodyColorMat[2];
+        currentMat = renderer.material;
+        }
+        public void yellowColor()
+        {
+        renderer.material = bodyColorMat[3];
+        currentMat = renderer.material;
+    }
+    
+
+}
---
+++ Assets/Script/InfoScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoScript : MonoBehaviour
+{
+    public  GameObject gb;
+    public bool activeme;
+    // Start is called before the first frame update
+    void Start()
+    {
+       // GameObject.FindWithTag("Image");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (activeme==true)
+            {
+                gb.SetActive(true);
+            }
+            else
+           {
+               gb.SetActive(false);
+           }
+        }
+        
+            
+
+
+
+        
+        
+    }
+}
